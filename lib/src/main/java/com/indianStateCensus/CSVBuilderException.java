f125c5c01251c1b94eab9b77df2505f28bc17f24
aditya Kharade <adityakharade51@gmail.com>
2020-10-24 23:18:15 +0530

    Refactored code to add CSV Exception to encapsulate CSV builder

--- lib/src/main/java/com/indianStateCensus/CSVBuilderException.java
+++ lib/src/main/java/com/indianStateCensus/CSVBuilderException.java
@@ -1,13 +1,15 @@
 package com.indianStateCensus;
-public class CensusAnalyserException extends Exception {
+
+public class CSVBuilderException extends Exception {
 	public enum ExceptionType {
 		NO_FILE, INCORRECT_FILE, UNABLE_TO_PARSE
 	}
 
 	public ExceptionType type;
 
-	public CensusAnalyserException(String message, ExceptionType type) {
+	public CSVBuilderException(String message, ExceptionType type) {
 		super(message);
 		this.type = type;
 	}
+
 }
